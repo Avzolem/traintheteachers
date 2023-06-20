a506dcce96f2ef506e4c2616f26e229ed5c9c4b7
Bart Lopez <dalopez@uach.mx>
2023-06-20 14:57:16 -0600

    white logos

--- pages/index.js
+++ pages/index.js
@@ -297,14 +297,14 @@ export default function Example() {
             <div className="mx-auto grid max-w-lg grid-cols-2 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-3 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-3">
               <img
                 className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-                src="https://res.cloudinary.com/djlx5smmq/image/upload/v1683660688/ciet_i1h9p6.png"
+                src="/images/logo-ciet-blanco.png"
                 alt="ciet"
                 width={158}
                 height={48}
               />
               <img
-                className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-                src="https://res.cloudinary.com/djlx5smmq/image/upload/v1683660688/logo-uach_xce9py.png"
+                className="col-span-2 max-h-16 w-full object-contain lg:col-span-1"
+                src="/images/+uach.png"
                 alt="+uach"
                 width={158}
                 height={48}
