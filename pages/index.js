346508b93327ec96821063e8fe4e4f42d0f7f5ee
Bart Lopez <dalopez@uach.mx>
2023-06-23 13:33:40 -0600

    rounded-md buttons

--- pages/index.js
+++ pages/index.js
@@ -278,7 +278,7 @@ export default function Example() {
                 <div className="mt-10 flex items-center justify-center gap-x-6">
                   <a
                     href="#"
-                    className="rounded-3xl bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
+                    className="rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
                   >
                     Ver cursos
                   </a>
