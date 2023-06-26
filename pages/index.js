f431f8797c13a7d227c6589b21d222634ee9b9e2
Bart Lopez <dalopez@uach.mx>
2023-06-26 10:43:24 -0600

    buttons rounded-sm

--- pages/index.js
+++ pages/index.js
@@ -344,7 +344,7 @@ export default function Example() {
                 desafíos del aprendizaje del Siglo XXI. ¿Crees en el poder del
                 cambio y la creatividad?
               </p>
-              <button className="mt-8 bg-primary px-6 py-3 rounded-md text-white font-semibold shadow-sm hover:bg-softgray focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400">
+              <button className="mt-8 bg-primary px-6 py-3 rounded-sm text-white font-semibold shadow-sm hover:bg-softgray focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400">
                 Toma tu primer curso
               </button>
             </div>
@@ -382,7 +382,7 @@ export default function Example() {
               ))}
             </dl>
             <div className="flex justify-center pt-8">
-              <button className="mt-8 bg-primary px-6 py-3 rounded-md text-white font-semibold shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400">
+              <button className="mt-8 bg-primary px-6 py-3 rounded-sm text-white font-semibold shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400">
                 Toma tu primer curso
               </button>
             </div>
